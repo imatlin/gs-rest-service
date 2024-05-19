256a5b521d049c34cc86eff5203e09639de3e803
Igor Matlin <github@matlin.org>
2024-05-19 20:04:40 +0000

    Added DT Java SDK integration

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,12 +2,25 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.datatheorem.ApiProtectServletFilter;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+	@Bean
+	FilterRegistrationBean<ApiProtectServletFilter> apiProtectFilterRegistrationBean() {
+		// registers api protect filter
+		FilterRegistrationBean<ApiProtectServletFilter> apiProtectFilterRegistrationBean = new FilterRegistrationBean<>();
+		apiProtectFilterRegistrationBean.addUrlPatterns("*");
+		apiProtectFilterRegistrationBean.setFilter(new ApiProtectServletFilter());
+		return apiProtectFilterRegistrationBean;
 	}
 
 }
